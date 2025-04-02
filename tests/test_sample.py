300f33e89dff5dff2ed9c8b88a91f94b6e38b808
tikkaaa3 <tikkaaa3@gmail.com>
2025-04-02 10:31:19 +0200

    test_sample.py updated

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -11,6 +11,10 @@ def test_sample_data(sample_data):
     assert sample_data["age"] == 30
 
 
-# @pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0), (0, 0, 0)])
-# def test_add_param(a, b, expected):
-#    assert add(a, b) == expected
+def add(a, b):
+    return a+b
+
+
+@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0), (0, 0, 0)])
+def test_add_param(a, b, expected):
+    assert add(a, b) == expected
